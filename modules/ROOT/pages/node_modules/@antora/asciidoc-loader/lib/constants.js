105f1629f6ff3d7a93ccbff42ac417d936508f90
Joan Edwards <joaedwar@redhat.com>
2019-07-15 20:46:53 +0100

    remove unnecessary files

--- modules/ROOT/pages/node_modules/@antora/asciidoc-loader/lib/constants.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.exports = Object.freeze({
-  $Antora () {},
-  EXAMPLES_DIR_TOKEN: 'example$',
-  PARTIALS_DIR_TOKEN: 'partial$',
-})
--- modules/ROOT/pages/node_modules/@antora/page-composer/lib/constants.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-module.exports = Object.freeze({
-  DEFAULT_LAYOUT_NAME: 'default',
-  HANDLEBARS_COMPILE_OPTIONS: Object.freeze({ preventIndent: true }),
-})
