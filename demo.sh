0014470d15cc60d2fc24b71f0486f8b5fff15dc1
praveen4g0 <pthangad@redhat.com>
2021-04-09 16:57:33 +0530

    Update deployment name

--- demo.sh
+++ demo.sh
@@ -120,7 +120,7 @@ demo.run() {
   info "Running API Build and deploy"
   TKN pipeline start build-and-deploy \
     -w name=shared-workspace,volumeClaimTemplateFile=01_pipeline/03_persistent_volume_claim.yaml \
-    -p deployment-name=vote-api \
+    -p deployment-name=pipelines-vote-api \
     -p git-url=https://github.com/openshift/pipelines-vote-api.git \
     -p IMAGE=image-registry.openshift-image-registry.svc:5000/pipelines-tutorial/vote-api \
     --showlog=true
@@ -128,7 +128,7 @@ demo.run() {
   info "Running UI Build and deploy"
   TKN pipeline start build-and-deploy \
     -w name=shared-workspace,volumeClaimTemplateFile=01_pipeline/03_persistent_volume_claim.yaml \
-    -p deployment-name=vote-ui \
+    -p deployment-name=pipelines-vote-ui \
     -p git-url=https://github.com/openshift/pipelines-vote-ui.git \
     -p IMAGE=image-registry.openshift-image-registry.svc:5000/pipelines-tutorial/vote-ui \
     --showlog=true
